c384e0da346a55ababd065f3663f669b01941dbd
笨熊 <bxczp@hotmail.com>
2020-10-16 16:50:00 +0800

    Create ExcelDataVO.java
    ExcelDataVO

---
+++ src/ExcelDataVO.java
@@ -0,0 +1,41 @@
+
+public class ExcelDataVO {
+
+    private String cont1;
+    private String cont2;
+    private String cont3;
+    private String cont4;
+
+    public String getCont1() {
+        return cont1;
+    }
+
+    public void setCont1(String cont1) {
+        this.cont1 = cont1;
+    }
+
+    public String getCont2() {
+        return cont2;
+    }
+
+    public void setCont2(String cont2) {
+        this.cont2 = cont2;
+    }
+
+    public String getCont3() {
+        return cont3;
+    }
+
+    public void setCont3(String cont3) {
+        this.cont3 = cont3;
+    }
+
+    public String getCont4() {
+        return cont4;
+    }
+
+    public void setCont4(String cont4) {
+        this.cont4 = cont4;
+    }
+
+}
